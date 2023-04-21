16e25bfe0f5c932a173fabc4ef46162bbb17055d
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-04-21 23:17:09 +0200

    feat: twitter opengraph metas

--- pages/analisis/[slug]/index.js
+++ pages/analisis/[slug]/index.js
@@ -1,9 +1,9 @@
-import Layout from '@/components/Layout';
-import styles from '@/styles/Review.module.css'
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import axios from 'axios';
-import { markdownToHtml, postFormatDate} from '@/utils'
+import Layout from "@/components/Layout";
+import styles from "@/styles/Review.module.css";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { markdownToHtml, postFormatDate } from "@/utils";
 import Image from "next/image";
 import Link from "next/link";
 import metascoreLogo from "@/public/metascore-logo.png";
@@ -11,71 +11,137 @@ import imdbLogo from "@/public/imdb-logo.png";
 import YoutubeVideo from "@/components/YoutubeVideo";
 
 const Review = ({ review }) => {
+  const { isFallback } = useRouter();
 
-    const { isFallback } = useRouter();
+  if (isFallback) {
+    return <div>Cargando...</div>;
+  }
 
-    if (isFallback) {
-        return <div>Cargando...</div>;
-    }
+  const body = markdownToHtml(review.attributes.description);
 
-    const body = markdownToHtml(review.attributes.description);
+  return (
+    <Layout>
+      <Head>
+        <title>{`Análisis de ${review.attributes.title} | TopDelMes`}</title>
+        <meta
+          name="description"
+          content={`Descubre el análisis de ${review.attributes.title}. Lee nuestra crítica detallada y entérate de por qué no te puedes perder.`}
+        />
+        <meta
+          name="keywords"
+          content={`análisis ${review.attributes.title}, crítica ${review.attributes.title}, reseña ${review.attributes.title}, serie ${review.attributes.title}, comedia, tribunal, opinión ${review.attributes.title}, calificación ${review.attributes.title}, TopDelMes`}
+        />
+        <link
+          rel="canonical"
+          href={`https://www.topdelmes.com/analisis/${review.attributes.slug}`}
+        />
 
-    return (
-        <Layout>
-            <Head>
-                <title>{`Análisis de ${review.attributes.title} | TopDelMes`}</title>
-                <meta name="description" content={`Descubre el análisis de ${review.attributes.title}. Lee nuestra crítica detallada y entérate de por qué no te puedes perder.`} />
-                <meta name="keywords" content={`análisis ${review.attributes.title}, crítica ${review.attributes.title}, reseña ${review.attributes.title}, serie ${review.attributes.title}, comedia, tribunal, opinión ${review.attributes.title}, calificación ${review.attributes.title}, TopDelMes`} />
-                <link rel="canonical" href={`https://www.topdelmes.com/analisis/${review.attributes.slug}`} />
-            </Head>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@topdelmes_" />
+        <meta
+          name="twitter:title"
+          content={`Análisis de ${review.attributes.title} | TopDelMes`}
+        />
+        <meta
+          name="twitter:description"
+          content={`Descubre el análisis de ${review.attributes.title}. Lee nuestra crítica detallada y entérate de por qué no te puedes perder.`}
+        />
+        <meta name="twitter:image" content={review.attributes.cover_url} />
 
-            <nav className={styles.breadcrumb}>
-                <i>»</i> <Link href="/analisis">Análisis</Link>
-            </nav>
-            <article className={styles.article} itemScope itemType="http://schema.org/Article">
-                <h1 itemProp="headline">
-                    {review.attributes.title}
-                </h1>
-                <div className={styles.pre}>
-                    <time dateTime={review.attributes.publishedAt} itemProp="datePublished">{postFormatDate(review.attributes.publishedAt)}</time>
-                    <p itemProp="author" itemScope itemType="http://schema.org/Person">
-                        <span itemProp="name">Oscar Bustos</span>
-                    </p>
-                </div>
-                <figure className={styles.figure}>
-                    <Image itemProp="image" src={review.attributes.cover_url} width={862} height={465} alt={`Análisis ${review.attributes.title}`} />
-                </figure>
-                <div className={styles.data}>
-                    {review.attributes.metascore && <p><Image src={metascoreLogo} width={16} height={16} alt='Metascore Logo' />{review.attributes.metascore}</p>}
-                    <p><Image src={imdbLogo} width={30} height={30} alt='IMDB Logo' />{review.attributes.imdb}</p>
-                </div>
-                <div itemprop="articleBody" className={styles.body} dangerouslySetInnerHTML={{ __html: body }} />
-                <YoutubeVideo url={review.attributes.trailer_url} />
-            </article>
+        <meta
+          property="og:title"
+          content={`Análisis de ${review.attributes.title} | TopDelMes`}
+        />
+        <meta
+          property="og:description"
+          content={`Descubre el análisis de ${review.attributes.title}. Lee nuestra crítica detallada y entérate de por qué no te puedes perder.`}
+        />
+        <meta property="og:image" content={review.attributes.cover_url} />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:url"
+          content={`https://www.topdelmes.com/analisis/${review.attributes.slug}`}
+        />
+        <meta property="og:site_name" content="TopDelMes" />
+      </Head>
 
-        </Layout>
-    );
+      <nav className={styles.breadcrumb}>
+        <i>»</i> <Link href="/analisis">Análisis</Link>
+      </nav>
+      <article
+        className={styles.article}
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <h1 itemProp="headline">{review.attributes.title}</h1>
+        <div className={styles.pre}>
+          <time
+            dateTime={review.attributes.publishedAt}
+            itemProp="datePublished"
+          >
+            {postFormatDate(review.attributes.publishedAt)}
+          </time>
+          <p itemProp="author" itemScope itemType="http://schema.org/Person">
+            <span itemProp="name">Oscar Bustos</span>
+          </p>
+        </div>
+        <figure className={styles.figure}>
+          <Image
+            itemProp="image"
+            src={review.attributes.cover_url}
+            width={862}
+            height={465}
+            alt={`Análisis ${review.attributes.title}`}
+          />
+        </figure>
+        <div className={styles.data}>
+          {review.attributes.metascore && (
+            <p>
+              <Image
+                src={metascoreLogo}
+                width={16}
+                height={16}
+                alt="Metascore Logo"
+              />
+              {review.attributes.metascore}
+            </p>
+          )}
+          <p>
+            <Image src={imdbLogo} width={30} height={30} alt="IMDB Logo" />
+            {review.attributes.imdb}
+          </p>
+        </div>
+        <div
+          itemprop="articleBody"
+          className={styles.body}
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
+        <YoutubeVideo url={review.attributes.trailer_url} />
+      </article>
+    </Layout>
+  );
 };
 
 export async function getServerSideProps(context) {
-    const { slug } = context.params;
+  const { slug } = context.params;
 
-    try {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL_POST}/posts?filters[slug][$eq]=${slug}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY_POST}`,
+        },
+      }
+    );
 
-        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL_POST}/posts?filters[slug][$eq]=${slug}`, {
-            headers: {
-                Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY_POST}`,
-            }
-        });
+    const review = response.data.data[0];
 
-        const review = response.data.data[0];
-
-
-        return { props: { review } };
-    } catch (error) {
-        console.error('Error al obtener los datos:', error);
-        return { props: { review: null } };
-    }
+    return { props: { review } };
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    return { props: { review: null } };
+  }
 }
 
 export default Review;
