0cbdc33ba0fda798554a7885d72deb3271e25a4b
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-06-09 10:58:47 +0200

    feat: add rate on cta

--- components/Card.js
+++ components/Card.js
@@ -84,7 +84,7 @@ const Card = ({ item, ranking }) => {
                 className={styles.link}
                 href={`/analisis/${review.attributes.slug}`}
               >
-                Leer análisis
+                Leer análisis: {review.attributes.rate}/10
               </Link>
             ))
           : null}
