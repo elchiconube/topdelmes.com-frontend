2828f2dc120f859e57feb107fd5f9aa012c2183a
Oscar Bustos <oscarbustosgarrido@gmail.com>
2023-07-12 16:09:31 +0200

    feat: add chat

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,11 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <body>
+        <iframe
+          src="https://chatfast.io/chat/bb136b44-fee5-4cce-acda-91af014d529b"
+          width="450px"
+          height="600px"
+        />
         <Main />
         <NextScript />
       </body>
